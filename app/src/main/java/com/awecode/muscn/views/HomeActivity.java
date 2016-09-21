768799e625cfe4129442799556b5c7292909f8d2
munnadroid <gotameraju2@gmail.com>
2016-09-21 18:34:19 +0545

    home view layout modified

--- app/src/main/java/com/awecode/muscn/views/HomeActivity.java
+++ app/src/main/java/com/awecode/muscn/views/HomeActivity.java
@@ -1,16 +1,47 @@
 package com.awecode.muscn.views;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.awecode.muscn.R;
+import com.awecode.muscn.model.CountDownTime;
+import com.awecode.muscn.util.Util;
+import com.awecode.muscn.util.countdown_timer.CountDownTimer;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends BaseActivity {
+    private static final String TAG = HomeActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        configureCountDownTimer();
 
+    }
+
+    private void configureCountDownTimer() {
+        CountDownTimer timer = new CountDownTimer();
+        timer.setOnTimerListener(new CountDownTimer.TimerListener() {
+            @Override
+            public void onTick(long millisUntilFinished, CountDownTime countDownTime) {
+                Log.v(TAG, "time ticked val: " + Util.getTwoDigitNumber(countDownTime.getDays()) + ":" + Util.getTwoDigitNumber(countDownTime.getHours()) + ":" + Util.getTwoDigitNumber(countDownTime.getMinutes())
+                        + ":" + Util.getTwoDigitNumber(countDownTime.getSeconds()));
+            }
+
+            @Override
+            public void onFinish() {
+            }
+
+        });
+        timer.setTime(500000000);
+    }
+
+    @Override
+    protected int getLayoutResourceId() {
+        return R.layout.activity_home;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
     }
 }
